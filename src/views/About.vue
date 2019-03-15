2d079be04f3d6544ef9a73a2ed029c6e9b9f39bd
pedro1191 <pedro_almeida1191@hotmail.com>
2019-03-15 08:46:58 -0300

    Update about page text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>Generic Website Admin App</h1>
+    <p>This app was developed using Vue JS.<p>
+    <p>It comunicates with a back end RESTful API developed in PHP using Laravel/Lumen.</p>
     <p>
       <strong>Version:</strong> 0.1.0
     </p>
