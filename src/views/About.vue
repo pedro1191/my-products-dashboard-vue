bacd77722cc3935ba2179c1e15c1121c614d548f
pedro1191 <pedro_almeida1191@hotmail.com>
2019-03-23 15:08:45 -0300

    Update about content

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,6 +6,22 @@
     <p>
       <strong>Version:</strong> 0.1.0
     </p>
+    <p>
+      <a
+        href="https://github.com/pedro1191/my-products-dashboard-vue.git"
+        target="_blank"
+        rel="noopener">
+        Vue JS App source code
+      </a>
+    </p>
+    <p>
+      <a
+        href="https://github.com/pedro1191/my-products-dashboard-rest.git"
+        target="_blank"
+        rel="noopener">
+        Laravel/Lumen RESTful API source code
+      </a>
+    </p>
   </div>
 </template>
 
