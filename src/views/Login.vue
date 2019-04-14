91bd3bb6185bfe754cad56e188ec37bb2a9b2d9b
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-14 15:10:40 -0300

    Update general style

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="login">
+
     <div class="card">
       <div class="card-header">
         <gws-logo :customStyle="customLogoStyle"></gws-logo>
@@ -8,74 +9,53 @@
         <form>
           <div class="form-group">
             <label for="email">Email</label>
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              v-model.trim="$v.form.email.$model"
-              :class="{ 'is-invalid': ($v.form.email.$error || formValidationMessages.email) }">
-            <div
-              class="invalid-feedback"
-              v-if="formValidationMessages.email">
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" v-model.trim="$v.form.email.$model" :class="{ 'is-invalid': ($v.form.email.$error || formValidationMessages.email) }">
+            <div class="invalid-feedback" v-if="formValidationMessages.email">
               {{ formValidationMessages.email }}
             </div>
-            <small
-              id="emailHelp"
-              class="text-muted">
+            <small id="emailHelp" class="text-muted">
               The default email is <strong>admin@myproducts.com</strong>.
             </small>
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              aria-describedby="passwordHelp"
-              v-model.trim="$v.form.password.$model"
-              :class="{ 'is-invalid': ($v.form.password.$error || formValidationMessages.password) }">
-            <div
-              class="invalid-feedback"
-              v-if="formValidationMessages.password">
+            <input type="password" class="form-control" id="password" aria-describedby="passwordHelp" v-model.trim="$v.form.password.$model" :class="{ 'is-invalid': ($v.form.password.$error || formValidationMessages.password) }">
+            <div class="invalid-feedback" v-if="formValidationMessages.password">
               {{ formValidationMessages.password }}
             </div>
-            <small
-              id="passwordHelp"
-              class="text-muted">
+            <small id="passwordHelp" class="text-muted">
               The default password is <strong>admin123</strong>.
             </small>
           </div>
-          <button
-            type="submit"
-            class="btn btn-outline-primary"
-            :disabled="$v.$invalid"
-            @click.prevent="onSubmit">
+          <button type="submit" class="btn btn-outline-secondary" :disabled="$v.$invalid" @click.prevent="onSubmit">
             Log in
           </button>
         </form>
       </div>
     </div>
+
     <gws-modal v-if="modal.error">
-      <div slot="header">My Products</div>
+      <div slot="header">My Food</div>
       <div slot="body">{{ modal.message }}</div>
-      <button class="btn btn-primary" @click="onModalClose" slot="footer">OK</button>
+      <button class="btn btn-secondary" @click="onModalClose" slot="footer">OK</button>
     </gws-modal>
+
     <gws-modal v-if="modal.loading">
       <gws-spinner slot="body"></gws-spinner>
     </gws-modal>
+
   </div>
 </template>
 
 <script>
-import axios from '@/axios-default'
-import { required, email } from 'vuelidate/lib/validators'
-import Logo from '@/components/Logo.vue'
-import Modal from '@/components/Modal.vue'
-import Spinner from '@/components/Spinner.vue'
+import axios from '@/axios-default';
+import { required, email } from 'vuelidate/lib/validators';
+import Logo from '../components/Logo.vue';
+import Modal from '../components/Modal.vue';
+import Spinner from '../components/Spinner.vue';
 
 export default {
-  data () {
+  data() {
     return {
       form: {
         email: 'admin@myproducts.com',
@@ -93,72 +73,75 @@ export default {
         error: false,
         message: null
       }
-    }
+    };
   },
   computed: {
-    formEmail () {
-      return this.form.email
+    formEmail() {
+      return this.form.email;
     },
-    formPassword () {
-      return this.form.password
+    formPassword() {
+      return this.form.password;
     }
   },
   watch: {
-    formEmail: function () {
+    formEmail: function() {
       if (!this.$v.form.email.email) {
-        this.formValidationMessages.email = 'The email must be a valid email address.'
+        this.formValidationMessages.email =
+          'The email must be a valid email address.';
       } else if (!this.$v.form.email.required) {
-        this.formValidationMessages.email = 'The email field is required.'
+        this.formValidationMessages.email = 'The email field is required.';
       } else {
-        this.formValidationMessages.email = null
+        this.formValidationMessages.email = null;
       }
     },
-    formPassword: function () {
+    formPassword: function() {
       if (!this.$v.form.password.required) {
-        this.formValidationMessages.password = 'The passowrd field is required.'
+        this.formValidationMessages.password =
+          'The passowrd field is required.';
       } else {
-        this.formValidationMessages.password = null
+        this.formValidationMessages.password = null;
       }
     }
   },
   methods: {
-    onSubmit () {
-      this.modal.loading = true
+    onSubmit() {
+      this.modal.loading = true;
 
-      axios.post('/auth/login', this.form)
+      axios
+        .post('/auth/login', this.form)
         .then(response => {
-          this.$store.dispatch('login', response.data)
-          this.modal.loading = false
-          this.$router.push({ name: 'home' })
+          this.$store.dispatch('login', response.data);
+          this.modal.loading = false;
+          this.$router.push({ name: 'home' });
         })
         .catch(error => {
-          this.onHttpRequestError(error)
-        })
+          this.onHttpRequestError(error);
+        });
     },
-    onHttpRequestError (error) {
-      this.modal.loading = false
-      this.modal.error = true
-      console.log(error.response)
+    onHttpRequestError(error) {
+      this.modal.loading = false;
+      this.modal.error = true;
+      console.log(error.response);
 
       switch (error.response.status) {
         case 422:
-          this.modal.message = error.response.data.message
+          this.modal.message = error.response.data.message;
 
-          const errors = error.response.data.errors
+          const errors = error.response.data.errors;
 
           for (let key in errors) {
-            this.formValidationMessages[key] = errors[key].join(' ')
+            this.formValidationMessages[key] = errors[key].join(' ');
           }
 
-          break
+          break;
         default:
-          this.modal.message = 'Oops! Something went wrong.'
+          this.modal.message = 'Oops! Something went wrong.';
       }
     },
-    onModalClose () {
-      this.modal.loading = false
-      this.modal.error = false
-      this.modal.message = null
+    onModalClose() {
+      this.modal.loading = false;
+      this.modal.error = false;
+      this.modal.message = null;
     }
   },
   validations: {
@@ -177,7 +160,7 @@ export default {
     gwsModal: Modal,
     gwsSpinner: Spinner
   }
-}
+};
 </script>
 
 <style scoped>
