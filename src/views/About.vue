83b6d238715e330b573577012e696f3f5a0b83d4
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-14 15:13:49 -0300

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,27 +1,21 @@
 <template>
   <div class="about">
-    <h1>My Products Dashboard</h1>
+    <h1>My Food Dashboard</h1>
     <p>This app was developed using Vue JS.<p>
-    <p>It comunicates with a back end RESTful API developed in PHP using Laravel/Lumen.</p>
-    <p>
-      <strong>Version:</strong> 0.1.0
-    </p>
-    <p>
-      <a
-        href="https://github.com/pedro1191/my-products-dashboard-vue.git"
-        target="_blank"
-        rel="noopener">
-        Vue JS App source code
-      </a>
-    </p>
-    <p>
-      <a
-        href="https://github.com/pedro1191/my-products-dashboard-rest.git"
-        target="_blank"
-        rel="noopener">
-        Laravel/Lumen RESTful API source code
-      </a>
-    </p>
+        <p>It comunicates with a back end RESTful API developed in PHP using Laravel/Lumen.</p>
+        <p>
+          <strong>Version:</strong> 0.1.0
+        </p>
+        <p>
+          <a href="https://github.com/pedro1191/my-products-dashboard-vue.git" target="_blank" rel="noreferrer noopener">
+            See source code of this Vue JS App
+          </a>
+        </p>
+        <p>
+          <a href="https://github.com/pedro1191/my-products-dashboard-rest.git" target="_blank" rel="noreferrer noopener">
+            See source code of the Laravel/Lumen RESTful API
+          </a>
+        </p>
   </div>
 </template>
 
@@ -30,5 +24,9 @@
   padding-top: 15px;
   padding-bottom: 15px;
   text-align: center;
+}
+
+h1 {
+  margin: 1rem 0;
 }
 </style>
