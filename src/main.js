ebab7fa6b968a9e960f41b760d4f4bf9af473ae2
Pedro Almeida <pedro_almeida1191@hotmail.com>
2022-07-02 14:49:13 -0300

    style: add backward icon

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import VueGtag from 'vue-gtag';
 
+library.add(faBackward);
+
 createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .use(store)
   .use(router)
   .use(VueGtag, {
